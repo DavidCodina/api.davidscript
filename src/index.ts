be8388b8a7c420aa95c8d5a95baac9221c601619
David Codina <davidmcodina@icloud.com>
2024-11-11 15:04:48 -0700

    Refactored cicd.yml

--- src/index.ts
+++ src/index.ts
@@ -76,7 +76,7 @@ app.get('/', (req, res) => {
       secret: process.env.SECRET || '???',
       NODE_ENV: process.env.NODE_ENV || '???'
     },
-    message: `You accessed the '/' route (CICD Test 1).`,
+    message: `You accessed the '/' route (CICD Test 2).`,
     success: true
   })
 })

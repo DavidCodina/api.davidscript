520c99fff24177ca6d5f1c9534a56ac5adce6f9f
David Codina <davidmcodina@icloud.com>
2024-11-12 14:44:45 -0700

    Update to success job

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,7 @@ app.get('/', (req, res) => {
       secret: process.env.SECRET || '???',
       NODE_ENV: process.env.NODE_ENV || '???'
     },
-    message: `You accessed the '/' route. 😎`,
+    message: `You accessed the '/' route! 🤪`,
     success: true
   })
 })

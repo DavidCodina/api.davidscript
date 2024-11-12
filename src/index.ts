ff5684ad0bbe54f74c012888de049c001494c3ea
David Codina <davidmcodina@icloud.com>
2024-11-12 15:16:58 -0700

    Another success fix

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,7 @@ app.get('/', (req, res) => {
       secret: process.env.SECRET || '???',
       NODE_ENV: process.env.NODE_ENV || '???'
     },
-    message: `You accessed the '/' route! 🤩`,
+    message: `You accessed the '/' route!!!`,
     success: true
   })
 })

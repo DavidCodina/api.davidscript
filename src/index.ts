f28a29f0c798660cfa93e75bc20784fa623c6aad
David Codina <davidmcodina@icloud.com>
2024-11-13 14:56:38 -0700

    Changed caching implementation

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,7 @@ app.get('/', (req, res) => {
       secret: process.env.SECRET || '???',
       NODE_ENV: process.env.NODE_ENV || '???'
     },
-    message: `You accessed the '/' route. ⚡️⚡️⚡️`,
+    message: `You accessed the '/' route. 👍🏻`,
     success: true
   })
 })
